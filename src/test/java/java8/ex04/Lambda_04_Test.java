0d4fa6bacd0099c6d35e4ff5d23f9dcdccd46e4a
Alexandre MEUR <alex.meur@gmail.com>
2019-05-13 14:57:45 +0200

    Suite des lambdas. Exo 4 pas fini.

--- src/test/java/java8/ex04/Lambda_04_Test.java
+++ src/test/java/java8/ex04/Lambda_04_Test.java
@@ -1,6 +1,7 @@
 package java8.ex04;
 
 
+import java8.data.Account;
 import java8.data.Data;
 import java8.data.Person;
 import org.junit.Test;
@@ -17,15 +18,15 @@ public class Lambda_04_Test {
 
     // tag::interfaces[]
     interface GenericPredicate<T> {
-        // TODO
+        boolean predicate(T t);
     }
 
     interface GenericMapper<T, E> {
-        // TODO
+        E map(T t);
     }
 
     interface Processor<T> {
-        // TODO
+        void process(T t);
     }
     // end::interfaces[]
 
@@ -48,18 +49,28 @@ public class Lambda_04_Test {
         // tag::methods[]
         private FuncCollection<T> filter(GenericPredicate<T> predicate) {
             FuncCollection<T> result = new FuncCollection<>();
-            // TODO
+            
+            for(T t: list) {
+            	if(predicate.predicate(t)) {
+            		result.add(t);
+            	}
+            }
             return result;
         }
 
         private <E> FuncCollection<E> map(GenericMapper<T, E> mapper) {
             FuncCollection<E> result = new FuncCollection<>();
-            // TODO
+            
+            for(T t : list) {
+            	result.add(mapper.map(t));
+            }
             return result;
         }
 
         private void forEach(Processor<T> processor) {
-           // TODO
+           for(T t: list) {
+        	   processor.process(t);
+           }
         }
         // end::methods[]
 
@@ -77,15 +88,20 @@ public class Lambda_04_Test {
 
         personFuncCollection
                 // TODO filtrer, ne garder uniquement que les personnes ayant un age > 50
-                .filter(null)
+                .filter(p -> p.getAge() > 50)
                 // TODO transformer la liste de personnes en liste de comptes. Un compte a par défaut un solde à 1000.
-                .map(null)
+                .map(p -> {
+                	Account account = new Account();
+                	account.setBalance(1000);
+                	account.setOwner(p);
+                	return account;
+                })
                 // TODO vérifier que chaque compte a un solde à 1000.
                 // TODO vérifier que chaque titulaire de compte a un age > 50
-                .forEach(null);
-
-        // TODO à supprimer
-        assert false;
+                .forEach(a -> {	
+                	assert(a.getBalance() == 1000);
+                	assert(a.getOwner().getAge() > 50);
+                });
     }
     // end::test_filter_map_forEach[]
 
@@ -99,11 +115,16 @@ public class Lambda_04_Test {
 
         // TODO créer un variable filterByAge de type GenericPredicate
         // TODO filtrer, ne garder uniquement que les personnes ayant un age > 50
-        // ??? filterByAge = ???;
+        GenericPredicate<Person> filterByAge = p -> p.getAge() > 50;
 
         // TODO créer un variable mapToAccount de type GenericMapper
         // TODO transformer la liste de personnes en liste de comptes. Un compte a par défaut un solde à 1000.
-        // ??? mapToAccount = ???;
+        GenericMapper<Person, Account> mapToAccount = person -> {
+        	Account account = new Account();
+        	account.setBalance(1000);
+        	account.setOwner(person);
+        	return account;
+        };
 
         // TODO créer un variable verifyAccount de type GenericMapper
         // TODO vérifier que chaque compte a un solde à 1000.
